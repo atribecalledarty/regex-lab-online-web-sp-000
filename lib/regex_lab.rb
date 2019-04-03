241f2d0adc0923d1ed5d466e7e6b518653cd824e
Arthur Song <arth3rs0ng@gmail.com>
2019-04-03 01:09:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,5 @@
 def starts_with_a_vowel?(word)
   word.match(/^[aeiouAEIOU]\w*/)
-  if word.match == nil
-    false
-  else
-    true
-  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
